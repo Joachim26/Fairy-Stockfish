0e1206dfb0b15af24fd6c4f35c3a0ce4073e938f
Fabian Fichter <ianfab@users.noreply.github.com>
2020-08-21 12:53:16 +0200

    tweak king danger

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -618,7 +618,7 @@ namespace {
 
     // For drop games, king danger is independent of game phase
     if (pos.captures_to_hand() || pos.two_boards())
-        score = make_score(mg_value(score), mg_value(score)) * 2 / (2 + 16 / pos.max_rank() * !pos.shogi_doubled_pawn());
+        score = make_score(mg_value(score), mg_value(score)) * 2 / (2 + 8 / pos.max_rank() * !pos.shogi_doubled_pawn());
 
     if (T)
         Trace::add(KING, Us, score);
